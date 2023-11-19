6258cbeab17f8643671369d76ba3dafa90fb958c
Omar Mahjoubi <omar.mahjoubi@infor.com>
2023-11-19 18:39:53 +0100

    My first commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* Configuration */
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
+app.use(morgan("common"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+
+
+/* Routes */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+/* Mongoose Setup */
+const PORT = process.env.PORT || 9000 ; 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true , 
+    useUnifiedTopology: true ,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
+
+
