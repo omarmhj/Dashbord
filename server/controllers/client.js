6e0f27415cd7508db1b8b9d2a33bc66c32f364e6
omar mahjoubi <omarmahjoubi@omars-MacBook-Pro.local>
2024-04-21 23:11:21 +0100

    fix some Bugs

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transactions.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -22,6 +23,15 @@ export const getProducts = async (req, res) => {
     } catch (error) {
       res.status(404).json({ message: error.message });
     }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 export const getTransactions = async (req, res) => {
